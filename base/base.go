beeb4dbc856e5c64100968020d94facbd748d913
Jormin <jorminxee@gmail.com>
2021-12-14 23:54:28 +0800

    :sparkles: feat(1.0.0): pkg
    Signed-off-by: Jormin <jorminxee@gmail.com>

--- base/base.go
+++ base/base.go
@@ -2,11 +2,27 @@ package base
 
 import (
 	"context"
+	"time"
 
 	"github.com/jormin/etcd-demo/pkg/etcd"
 	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
+// endpoints Endpoint列表
+var endpoints = []string{
+	"127.0.0.1:12379",
+	"127.0.0.1:22379",
+	"127.0.0.1:32379",
+}
+
+// config Etcd 配置
+var config = clientv3.Config{
+	Endpoints:            endpoints,
+	DialTimeout:          time.Second * 30,
+	DialKeepAliveTimeout: time.Second * 30,
+	Username:             "root",
+	Password:             "111111",
+}
 // cli Etcd客户端
 var cli *clientv3.Client
 
@@ -14,6 +30,6 @@ var cli *clientv3.Client
 var ctx context.Context
 
 func init() {
-	cli = etcd.GetEtcdClient()
+	cli = etcd.GetEtcdClient(config)
 	ctx = context.Background()
 }
--- pkg/etcd/etcd.go
+++ pkg/etcd/etcd.go
@@ -1,37 +1,14 @@
 package etcd
 
 import (
-	"time"
-
 	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
-// endpoints Endpoint列表
-var endpoints = []string{
-	"127.0.0.1:12379",
-	"127.0.0.1:22379",
-	"127.0.0.1:32379",
-}
-
-// config Etcd 配置
-var config = clientv3.Config{
-	Endpoints:            endpoints,
-	DialTimeout:          time.Second * 30,
-	DialKeepAliveTimeout: time.Second * 30,
-	Username:             "root",
-	Password:             "111111",
-}
-
 // GetEtcdClient 获取Etcd客户端
-func GetEtcdClient() *clientv3.Client {
-	cli, err := clientv3.New(config)
+func GetEtcdClient(cfg clientv3.Config) *clientv3.Client {
+	cli, err := clientv3.New(cfg)
 	if err != nil {
 		panic(err)
 	}
 	return cli
-}
-
-// GetConfig 获取配置
-func GetConfig() clientv3.Config {
-	return config
 }
